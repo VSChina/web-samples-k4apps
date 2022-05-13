5f68f729ba25b35615444bb1028af5eb76026ee9
layliu <layliu@microsoft.com>
2022-05-13 09:36:45 +0000

    Update workflow for githubactiontestvulifqex-stage

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello, World! " + process.versions.node);
+    response.end("vulifqex-stage" + process.versions.node);
 });
 
 const port = process.env.PORT || 1337;
